792975b1409b6e9ea891470c99961426ee263c99
Krishna N Acharya <knacharyakavoor@gmail.com>
2024-02-03 12:11:44 +0530

    uodated

---
+++ RockPaperScisssor.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+public class RockPaperScisssor {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Random rand= new Random();
+        System.out.println("Enter your choice. ");
+        
+}  
+  
+    
+}
---
+++ javascript workshop/idk.js
@@ -0,0 +1,68 @@
+// //let a={
+//     //'first_name': 'Jon',
+// //};
+// //const a=5;
+// //a=6;
+// //try {
+
+//   //  a={
+//      //   first_name:'Jon'
+//    // }
+// //}catch(err){
+//    // console.log(err.message);
+// //}
+
+// //console.log(typeof a);
+// //let a=false;
+// //console.log(typeof a);
+
+// // const a=['a','e','f'];
+// // const b=a.join(' -- ');
+// // let c='';
+// // for (i=0; i<a.length;i++){
+// //     c-c+a[i]+' == ';
+// // }
+// // console.log(b);
+// // console.log(typeof b);
+// // console.log(c);
+
+// // a.forEach(ele => {
+// //   ele=ele+'changed'
+// //   console.log(ele);
+// //   return ele;
+// // })
+
+// // comsole.log(a);
+// // console.log('--------');
+// // for( const ele of a){
+// //   console.log(ele);
+// // }
+
+// const user={
+//   firstName:'Krishna',
+//   lastName:'Acharya',
+//   age:20,
+//   address: {
+//     collegeName: 'NMAMIT',
+//     area:'NITTE',
+//     pincode:277842
+//   }
+// }
+// // if(user.age>20){
+// //   user,age+=5;
+// //   user.remark='Age must be greater than 20';
+// // } else {
+// //   user.age-=5;
+// //   user.remark='Age is perfect';
+// // }
+// // console.log(user)
+// user.age >20 ? user.age+= 5:user.age-=5;
+// console.log(user);
+
+const calculateAge=function (age) {
+  console.log(age);
+  return(age);
+}
+console.log(calculateAge(5000));
+
+//calculateAge(20);
